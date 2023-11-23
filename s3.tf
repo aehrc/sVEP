1be197035149f6faae70dbc760539a571246ae8a
Brendan Hosking <brendan.hosking@gmail.com>
2023-11-23 16:22:15 +1100

    FEAT: Add CORS for get requests to result bucket

--- s3.tf
+++ s3.tf
@@ -11,3 +11,15 @@ resource "aws_s3_bucket" "svep-results" {
   bucket_prefix = "svep-results"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_cors_configuration" "svep-results-cors" {
+  bucket = aws_s3_bucket.svep-results.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
